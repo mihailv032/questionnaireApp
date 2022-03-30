6bdeb0959323172c92845c476a3d7c904666820a
mihail <mishgun032@yandex.com>
2022-03-30 20:51:24 +0100

    yes

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import {readDirectoryAsync,documentDirectory,StorageAccessFramework} from 'expo-file-system';
+
+import Home from './components/home'
+import LvlContainer from './components/lvl'
+
+const Stack= createStackNavigator()
+
 export default function App() {
+  details = async () => {
+    let files = await readDirectoryAsync(documentDirectory)
+    files.includes('details.json') ? console.log('f') : StorageAccessFramework.createFileAsync(documentDirectory,'details.json')
+
+  }
+  details()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+	<Stack.Screen name="home" component={Home}/>
+	<Stack.Screen name="lvl" component={LvlContainer}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+async function details(){
+  
+}
+
